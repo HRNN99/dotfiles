a9abfdb659d9d63691ed5044569d9520a60e72a2
Hernan <villahrnn@hotmail.com>
2025-04-19 16:35:30 -0300

    file verify sha

--- shell/functions/terminal/file_utils.sh
+++ shell/functions/terminal/file_utils.sh
@@ -11,3 +11,13 @@ file_count_words() {
 file_count_characters() {
   cat $1 | wc -c
 }
+
+file_verify_hash(){
+    # if file is not entered 
+    if [[ -z "$1"]]; then
+        echo "Usage: file_verify_hash <file>"
+        return 1
+    fi
+
+    openssl dgst -sha256 $1
+}
